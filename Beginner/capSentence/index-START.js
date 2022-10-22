b0ec0228a70caa50164def15fa16cf4b5667490b
ElektrazCode <ElektrazCode@gmail.com>
2022-10-22 15:00:00 -0400

    Solved Capitalize Sentence Problem

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -7,10 +7,31 @@ return the equivalent of the sentence when capitalised. E.g
 
 
 
+// function capSentence(text) {
+//    return text.split(' ')
+//               .map(word => word[0].toUpperCase() + word.substring(1).toLowerCase())  //can also use .slice(1)
+//               .join(' ');
+// }
 function capSentence(text) {
-   // Code goes here
+  let sentence = text.toLowerCase().split(' ');
+   return sentence.map(word => word.replace(word[0], word[0].toUpperCase()))
+                  .join(' ');
 }
 
-
+// function capSentence(text) {
+//   let output = '';
+//   let i = 0;
+//   while(text[i]){
+//     let space = text.indexOf(' ', i+1) || text.length;
+//     output += text[i].toUpperCase() + text.substring(i+1, space).toLowerCase();
+//     i = space + 1;
+//   }
+//   return output;
+// }
+console.log(capSentence('the lion king'));
 
 module.exports = capSentence
+
+/**Performance: .map & .slice is the fastest solution -> forEach -> .map & .replace
+ * 
+*/
