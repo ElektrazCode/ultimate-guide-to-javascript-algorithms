e52850c7cd6103f6ba1036c1ce52f352895bb588
ElektrazCode <ElektrazCode@gmail.com>
2022-10-26 15:11:07 -0400

    Solved Max Recurring Character

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -5,10 +5,44 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 
 
 
+// function maxRecurringChar(text) {
+//     let letters = {};
+//     let maxChar;
+//     let max=0;
+//     for (let letter of text){
+//         if (letter in letters)  //if (letter.hasOwnProperty(letter)) or if (letters[letter])
+//             letters[letter]++;
+//         else
+//             letters[letter] = 1; 
+//     }
+//     for(let letter in letters){
+//         if(letters[letter]>max){
+//             max = letters[letter];
+//             maxChar = letter;
+//         }
+//     }
+//     return maxChar;
+// }
+
 function maxRecurringChar(text) {
-    // Code goes here
+    let letters = {};
+
+    for (let letter of text){
+        if (letter in letters)  //if (letter.hasOwnProperty(letter)) or if (letters[letter])
+            letters[letter]++;
+        else
+            letters[letter] = 1; 
+    }
+
+    let chars = Object.keys(letters);
+    let values = Object.values(letters);
+    let max = Math.max(...values);
+    return chars[values.indexOf(max)];
 }
 
 
-
 module.exports = maxRecurringChar;
+
+/**Performance-- Best to worst: for..in -> converting object into arrays methods
+ * 
+ */
