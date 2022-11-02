555de65b24b0d1b8fd56c4c3bd2132cee3a7eb7c
ElektrazCode <ElektrazCode@gmail.com>
2022-11-02 11:58:54 -0400

    Solved Anagram Check Problem

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -7,9 +7,27 @@ don't. E.g
 
 
 
+// function isAnagram(stringA, stringB) {
+//     let letters = {};
+//     for (let letter of stringA){
+//         if (letters[letter])
+//             letters[letter]++;
+//         else 
+//             letters[letter] = 1;
+//     }
+//     console.log(letters);
+//     for (let letter of Object.keys(letters)){   //filter will cost us o(n2) instead make 2 char maps one for each string and compare them.
+//         if ((stringB.split('').filter(l => l===letter).length || 0) !== letters[letter])
+//             return false;
+//     }
+//     return true;
+// }
+
 function isAnagram(stringA, stringB) {
-    // Code goes here
+    const sanitize = (str) => str.toLowerCase().match(/[a-z]/ig).sort().join('');
+    return sanitize(stringA) === sanitize(stringB);
 }
 
-
 module.exports = isAnagram
+
+/**Performance: char Map is faster than direct comparison */
