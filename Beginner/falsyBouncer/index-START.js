70a8dc2fc13078f08d333c3fdc2d8203e7559911
ElektrazCode <ElektrazCode@gmail.com>
2022-11-03 13:11:30 -0400

    Solved falsyBouncer

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -7,8 +7,10 @@ E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 
 
 function falsyBouncer(array) {
-    // Code goes here
+    return array.filter(element => element)
 }
 
 
 module.exports = falsyBouncer
+
+/**Performance: a for..of loop pushing truthy values to a new array would be slightly faster than the .filter method */
